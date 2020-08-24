77103517095fa6fd5c191dd93a1023afa362c751
liumt <943747060@qq.com>
2020-08-24 23:23:06 +0800

    搜索插入位置

---
+++ src/main/java/BinarySearch/question35.java
@@ -0,0 +1,40 @@
+package BinarySearch;
+
+/**
+ * @author ：liumt
+ * @date ：Created in 2020/8/24 23:20
+ * @description：给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * @modified By：
+ * @version: 1.1
+ */
+public class question35 {
+    /*
+     * @Author liumt
+     * @Description 直接套用二分法公式
+     * @Date 23:20 2020/8/24
+     * @Param [nums, target]
+     * @return int
+     **/
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            //获取中间位置
+            int mid = (left + right) >>> 1;
+            //相等直接返回了
+            if(nums[mid] == target){
+                return mid;
+            }
+            //固定写法,注意边界问题
+            if(target > nums[mid]){
+                left = mid + 1;
+            }
+            //固定写法,注意边界问题
+            if(target < nums[mid]){
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
