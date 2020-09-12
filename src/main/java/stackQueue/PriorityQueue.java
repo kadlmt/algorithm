3e18fb6322c75c7b25fdabae2de8211db8deadf9
liumt <943747060@qq.com>
2020-09-12 11:33:50 +0800

    使用最大堆实现最大优先队列

--- src/main/java/stackQueue/PriorityQueue.java
+++ src/main/java/stackQueue/PriorityQueue.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * @author ：liumt
  * @date ：Created in 2020/9/11 17:38
- * @description：优先队列,使用最大堆来实现
+ * @description：最大优先队列,使用最大堆来实现
  * @modified By：
  * @version: 1.1
  */
