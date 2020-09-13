809475559b6aa55002947a8c59c6e5fa5e15f36d
liumt <943747060@qq.com>
2020-09-14 07:47:33 +0800

    整数拆分

--- src/main/java/sort/CountSort.java
+++ src/main/java/sort/CountSort.java
@@ -34,9 +34,35 @@ public class CountSort {
         return sortArray;
     }
 
+    public static int[] countSort2(int[] array){
+        int max = array[0];
+        int min = array[0];
+        //统计最大值和最小值
+        for(int i = 0; i < array.length; i++){
+            if(array[i] > max) max = array[i];
+            if(array[i] < min) min = array[i];
+        }
+        int d = max - min;
+        int[] countArray = new int[d + 1];
+        //统计元素个数
+        for(int i = 0; i < array.length; i++){
+            countArray[array[i] - min] += 1;
+        }
+        //统计数组变形
+        for(int i = 1; i < countArray.length; i++){
+            countArray[i] += countArray[i - 1];
+        }
+        int[] sortArray = new int[array.length];
+        for(int i = array.length - 1; i >= 0; i--){
+            sortArray[countArray[array[i] - min] - 1] = array[i];
+            countArray[array[i] - min]--;
+        }
+        return sortArray;
+    }
+
     public static void main(String[] args) {
         int[] array = new int[] {4,4,6,5,3,2,8,1,7,5,6,0, 10};
-        int[] sortedArray = countSort(array);
+        int[] sortedArray = countSort2(array);
         System.out.println(Arrays.toString(sortedArray));
     }
 }
