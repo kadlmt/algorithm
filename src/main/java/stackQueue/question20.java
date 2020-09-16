f57566ea938f2fc6005bb10f5dc226c1f309f982
liumt <943747060@qq.com>
2020-09-16 12:22:40 +0800

    有效的括号

--- src/main/java/stackQueue/question20.java
+++ src/main/java/stackQueue/question20.java
@@ -21,13 +21,18 @@ public class question20 {
      * @return boolean
      **/
     public boolean isValid(String s) {
-        if(s.isEmpty()) return true;
+        if(s.isEmpty()) return false;
         Stack<Character> stack = new Stack<>();
         for(char c: s.toCharArray()){
-            if(c == '(') stack.push(')');
-            else if(c == '{') stack.push('}');
-            else if(c == '[') stack.push(']');
-            else if(stack.isEmpty() || c != stack.pop()) return false;
+            if(c == '('){
+                stack.push(')');
+            } else if(c == '['){
+                stack.push(']');
+            } else if(c == '{'){
+                stack.push('}');
+            } else if(stack.isEmpty() || c != stack.pop()){
+                return false;
+            }
         }
         return stack.isEmpty();
     }
