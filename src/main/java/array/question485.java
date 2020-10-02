5408f00e1153c64bc5140f106409d36130f23b3b
liumt <943747060@qq.com>
2020-10-02 20:48:43 +0800

    最大连续1的个数

--- src/main/java/array/question485.java
+++ src/main/java/array/question485.java
@@ -9,17 +9,16 @@ package array;
  */
 public class question485 {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int sum = 0;
-        int temp = 0;
-        int i = 0;
-        while(i < nums.length){
-            if(nums[i] == 1)temp++;
-            if(nums[i] == 0){
-                sum = Math.max(temp, sum);
-                temp = 0;
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+            } else{
+                max = Math.max(count, max);
+                count = 0;
             }
-            i++;
         }
-        return Math.max(temp, sum);
+        return Math.max(max, count);
     }
 }
