13fa88729a4de434c2bec5359392d5174bb24d5b
liumt <943747060@qq.com>
2020-10-02 08:42:52 +0800

    移动0

--- src/main/java/array/question283.java
+++ src/main/java/array/question283.java
@@ -8,6 +8,13 @@ package array;
  * @version: 1.1
  */
 public class question283 {
+    /**
+     * @Author liumt
+     * @Description 两次循环
+     * @Date 8:41 2020/10/2
+     * @Param [nums]
+     * @return void
+     **/
     public void moveZeroes(int[] nums) {
         int length = nums.length;
         //记录非0的个数
@@ -19,6 +26,24 @@ public class question283 {
         }
         for(int i = j; i < length; i++){
             nums[i] = 0;
+        }
+    }
+
+    /**
+     * @Author liumt
+     * @Description 一次循环,利用快排的思想,左边是非0,右边是0
+     * @Date 8:41 2020/10/2
+     * @Param [nums]
+     * @return void
+     **/
+    public void moveZeroes2(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
         }
     }
 }
