1f8b7567d89df8a87d02e7c34478b65e6df2d14f
liumt <943747060@qq.com>
2020-10-10 12:37:31 +0800

    相交的链表

--- src/main/java/linkedList/question160.java
+++ src/main/java/linkedList/question160.java
@@ -1,5 +1,8 @@
 package linkedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author ：liumt
  * @date ：Created in 2020/8/17 23:04
@@ -8,7 +11,27 @@ package linkedList;
  * @version: 1.1
  */
 public class question160 {
-    /*
+    /**
+     * @Author liumt
+     * @Description 散列法,不如双指针
+     * @Date 12:35 2020/10/10
+     * @Param [headA, headB]
+     * @return linkedList.ListNode
+     **/
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> hashSet = new HashSet<>();
+        while(headA != null){
+            hashSet.add(headA);
+            headA = headA.next;
+        }
+        while(headB != null){
+            if(hashSet.contains(headB)) return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    /**
      * @Author liumt
      * @Description 双指针方法.当pa==null,pa指向headB;pb==null,指向headA;
      * 当pa==pb,找到相交节点.
@@ -17,7 +40,7 @@ public class question160 {
      * @Param [headA, headB]
      * @return linkedList.ListNode
      **/
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
         ListNode pa = headA;
         ListNode pb = headB;
         while(pa != pb){
